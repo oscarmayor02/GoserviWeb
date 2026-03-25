cd4accbe44925197453bf52ec0e20eab6161833e
Oscar Mayor <oscar.mayor.dev@gmail.com>
2026-03-25 10:31:09 -0500

    chore: update gitignore and admin api service

--- src/app/web/dashboard/shared/admin-api.service.ts
+++ src/app/web/dashboard/shared/admin-api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../../../../environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class AdminApiService {
-  private base = "http://localhost:8080/admin";
+  private base = `${environment.apiUrl}/admin`;
 
   constructor(private http: HttpClient) {}
 
