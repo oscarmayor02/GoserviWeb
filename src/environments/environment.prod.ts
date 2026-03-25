bf2102c9021b625cca553a8f5f8895b0798207aa
Oscar Mayor <oscar.mayor.dev@gmail.com>
2026-03-25 11:54:34 -0500

    vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://goservi-clean-production.up.railway.app",
+  wsUrl: "https://goservi-clean-production.up.railway.app",
+  googleMapsApiKey: "AIzaSyAitJr1tiigXbnFRRxLyFvsXDqN5h2Vx9k",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:8080",
+  wsUrl: "http://localhost:8080",
+  googleMapsApiKey: "AIzaSyAitJr1tiigXbnFRRxLyFvsXDqN5h2Vx9k",
+};
